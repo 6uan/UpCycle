c1a6ff56c01c211d6b9f956e6a849a317cc96d9c
6uan <6uannn@gmail.com>
2023-11-30 03:43:42 -0500

    Refactoring using Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,43 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.{html,js}"
+],
+  theme: {
+
+    fontFamily: {
+      'ubuntu': ['Ubuntu', 'sans-serif'],
+    },
+
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'offwhite': '#dedede',
+      'white': '#ffffff',
+      'stonegray': '#C0C1B9',
+      'sage': '#9fa287',
+    },
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
+    extend: {},
+  },
+  plugins: [ 
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+  ],
+}
+
